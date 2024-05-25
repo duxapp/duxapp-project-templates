7786430dd66797e01514b4de696dd199de3d224b
徐健 <908634674@qq.com>
2024-05-25 16:14:34 +0800

    优化

--- default/babel.config.js
+++ default/babel.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-commonjs */
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-const bable = require('duxapp-cli/lib/bable')
+const bable = require('duxapp-cli/plugins/bable')
 const configs = require('./babel.user.config.js')
 
 module.exports = bable.merge({
