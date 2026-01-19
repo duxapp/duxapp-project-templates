185a0a8fe30cfdd0d8ba02a014761fc343b0abde
duxapp <908634674@qq.com>
2026-01-19 10:52:41 +0800

    update

--- apps/duxappCanvas/app.js
+++ apps/duxappCanvas/app.js
@@ -1,2 +1,4 @@
-// 禁止使用 NativeReanimatedContainer 绘制 会大幅降低性能
-global._skiaUseReanimated = false
+if (process.env.TARO_ENV === 'rn') {
+  // 禁止使用 NativeReanimatedContainer 绘制 会大幅降低性能
+  global._skiaUseReanimated = false
+}
